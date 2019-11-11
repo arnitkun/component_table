4138e5af9c17e826fd0dd100a0438047e39cd96d
arnitkun <ankit.nitagartala@gmail.com>
2019-11-11 13:17:22 +0530

    added container element for table

--- main.js
+++ main.js
@@ -43,5 +43,40 @@ function MyTable(options, containerElement) {
         table.appendChild(thead);
         this.containerElement.appendChild(table);
       };
-    
+      this.renderBody = () => {
+        var table = this.containerElement.childNodes[0];
+        let tbody = document.createElement("tbody");
+        this.data.forEach(rowData => { 
+          let trBody = document.createElement("tr");
+          trBody.setAttribute("id", rowData.id);
+          rowData.colData.forEach((datum, index) => {
+            let td = document.createElement("td");
+            if (this.colDefs[index].type === "text") {//logic for deciding cell type
+              td.innerText = datum;
+            } else if (this.colDefs[index].type === "input") {
+              const input = document.createElement("input");
+              input.setAttribute("type", "text");
+              input.setAttribute("value", datum);
+              input.checked = datum;
+              td.appendChild(input);
+            } else if (this.colDefs[index].type === "checkbox") {
+              const input = document.createElement("input");
+              input.setAttribute("type", "checkbox");
+              input.setAttribute("value", datum);
+              input.checked = datum;
+              td.appendChild(input);
+            }
+            td.style.width = this.colDefs[index].width;
+            trBody.appendChild(td);//append cell to row
+          });
+          tbody.appendChild(trBody);//append row to body
+        });
+        table.childNodes[1]//check if there is already a row. If yes, append new row else replace.
+          ? table.replaceChild(tbody, table.childNodes[1])
+          : table.appendChild(tbody);
+      };
 }
+
+const containerElement = document.getElementById("app");
+var table = new MyTable(options, containerElement);
+table.render();
